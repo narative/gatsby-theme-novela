c31693ff3f58159b09b0ecb4562887fe2c766cb6
DoLim <limdongyoung0@gmail.com>
2020-05-08 11:46:02 +0000

    fix: fix condition of shouldUpdateScroll.js

--- @narative/gatsby-theme-novela/src/gatsby/browser/shouldUpdateScroll.js
+++ @narative/gatsby-theme-novela/src/gatsby/browser/shouldUpdateScroll.js
@@ -5,10 +5,8 @@ module.exports = ({ routerProps, prevRouterProps, getSavedScrollPosition }) => {
   const topOfPage = [0, 0];
 
   if (routerProps.location.hash.includes('#fn')) {
-    return false;
-  }
-
-  if (routerProps.location.action === 'POP' && currentPosition) {
+    // Do Nothing
+  } else if (routerProps.location.action === 'POP' && currentPosition) {
     window.scrollTo(...currentPosition);
   } else {
     window.scrollTo(...topOfPage);
@@ -21,6 +19,5 @@ module.exports = ({ routerProps, prevRouterProps, getSavedScrollPosition }) => {
       prevRouterProps.location.pathname,
     );
   }
-  // FIXME: Make various test behaviour and make condition and test it!
   return false;
 };
