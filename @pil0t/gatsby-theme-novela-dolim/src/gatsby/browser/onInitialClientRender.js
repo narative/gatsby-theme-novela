027de26903b208b819b0116c48f2869a0d64a8db
DoLim <limdongyoung0@gmail.com>
2020-05-09 05:09:19 +0000

    fix: Now gatsby don't need @reach/router method

--- @pil0t/gatsby-theme-novela-dolim/src/gatsby/browser/onInitialClientRender.js
+++ @pil0t/gatsby-theme-novela-dolim/src/gatsby/browser/onInitialClientRender.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
 
-const { globalHistory } = require('@reach/router');
-
 function handleAccessibilityFocus() {
   const elementsWithA11yFocus = [...document.querySelectorAll('[data-a11y]')];
 
-  document.addEventListener('keyup', event => {
-    elementsWithA11yFocus.forEach(element => {
+  document.addEventListener('keyup', (event) => {
+    elementsWithA11yFocus.forEach((element) => {
       if (element === event.target || element.contains(event.target)) {
         element.setAttribute('data-a11y', 'true');
       } else {
@@ -16,8 +14,8 @@ function handleAccessibilityFocus() {
   });
 
   // On mouse click change data-a11y attribute false
-  document.addEventListener('mousedown', event => {
-    elementsWithA11yFocus.forEach(element => {
+  document.addEventListener('mousedown', (event) => {
+    elementsWithA11yFocus.forEach((element) => {
       if (element === event.target || element.contains(event.target)) {
         element.setAttribute('data-a11y', 'false');
       }
@@ -25,16 +23,16 @@ function handleAccessibilityFocus() {
   });
 }
 
-module.exports = () => {
+module.exports = async () => {
   localStorage.removeItem('previousPath');
 
   setTimeout(() => {
     handleAccessibilityFocus();
   }, 1000);
-
+  // await handleAccessibilityFocus();
   /**
    * This is a workaround for a bug in Gatsby
    * See https://github.com/gatsbyjs/gatsby/issues/8357 for more details
    */
-  globalHistory._onTransitionComplete();
+  // globalHistory._onTransitionComplete();
 };
