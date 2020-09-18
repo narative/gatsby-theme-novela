66561e130c03db4b91937f75f199d4a067820e8f
Kailash Soni <65533650+mail7@users.noreply.github.com>
2020-09-18 11:25:31 +0530

    Create Article.Comment.tsx

---
+++ @narative/gatsby-theme-novela/src/sections/article/Article.Comment.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { S9comment } from 'gatsby-plugin-social9-comment'
+
+interface ArticleCommentProps {}
+
+const ArticleComment: React.FC<ArticleCommentProps> = ({  }) => {
+  return (
+    <div className="css-xbcnqo-Paragraph"><S9comment /></div>
+  );
+};
+
+export default ArticleComment;
