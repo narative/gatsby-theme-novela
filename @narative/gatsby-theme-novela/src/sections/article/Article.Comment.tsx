3f078ad548877e49cc799516f2d64cfe564818df
Kailash Soni <65533650+mail7@users.noreply.github.com>
2020-09-28 11:20:09 +0530

    Update Article.Comment.tsx
    Add option for enable and disable commenting system on article

--- @narative/gatsby-theme-novela/src/sections/article/Article.Comment.tsx
+++ @narative/gatsby-theme-novela/src/sections/article/Article.Comment.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
-
 import { S9comment } from 'gatsby-plugin-social9-comment'
+import { graphql, useStaticQuery } from 'gatsby';
 
 interface ArticleCommentProps {}
 
-const ArticleComment: React.FC<ArticleCommentProps> = ({  }) => {
+const siteQuery = graphql`
+{
+  allSitePlugin(filter: {}) {
+    edges {
+      node {
+        pluginOptions {
+          enable
+        }
+        name
+      }
+    }
+  }
+}
+`;
+
+const ArticleComment: React.FC<ArticleCommentProps> = ({}) => {
+
+  const results = useStaticQuery(siteQuery);
+  const plugins = results.allSitePlugin.edges;
+  var is_enable = false;
+  for(var i in plugins){
+    if(plugins[i].node.name == "gatsby-plugin-social9-comment"){
+      is_enable = plugins[i].node.pluginOptions.enable
+    }
+  }
+  if(is_enable){
   return (
-    <div className="css-xbcnqo-Paragraph"><S9comment /></div>
+    <div className="css-xbcnqo-Paragraph"><S9comment/></div>
   );
+  }else{
+    return (
+      <div className="css-xbcnqo-Paragraph"></div>
+    );
+  }
 };
 
 export default ArticleComment;
